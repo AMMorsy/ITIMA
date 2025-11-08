d707341247aadd18c376e3a77dd42d035874a47e
Ahmed <a7med.morsy89@gmail.com>
2025-11-08 14:12:53 +0200

    feat: default dark theme

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -6,8 +6,8 @@ export function ThemeProvider({ children }: { children: React.ReactNode }) {
   return (
     <NextThemesProvider
       attribute="class"
-      defaultTheme="system"
-      enableSystem
+      defaultTheme="dark"      // <-- default is DARK
+      enableSystem={false}     // <-- ignore OS theme
       disableTransitionOnChange
     >
       {children}
